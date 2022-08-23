2df6648807ca317f1acc5151a2954cd3275845dc
abelasfaw <abeloman89@gmail.com>
2022-08-23 15:47:02 +0000

    fix indent

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10, 10)
 if number == 0:
-	message = "is zero"
+    message = "is zero"
 elif number > 0:
-	message = "is positive"
+    message = "is positive"
 else:
-	message = "is negative"
+    message = "is negative"
 print(f"{number} {message}")
