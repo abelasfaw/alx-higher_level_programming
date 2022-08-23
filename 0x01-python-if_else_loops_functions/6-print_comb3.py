070b20a39eb38c21c4befdaa08b3e24a40f7af3b
abelasfaw <abeloman89@gmail.com>
2022-08-23 19:54:01 +0000

    fix format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(((i * 10) + (i + 1)), (i * 10) + 10):
         if (j == 89):
-	    continue
+            continue
         print("{:02d}, ".format(j), end="")
 print("{:d}".format(j))
