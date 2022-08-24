291e3b018ef8fdef2a40a83e05f5533a71f0f88f
abelasfaw <abeloman89@gmail.com>
2022-08-24 18:42:14 +0000

    fix task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if (b == 0):
-        return 1
-    else:
-        result = 1
-        for i in range(0, b):
-            result *= a
-    return result
+    return a ** b
