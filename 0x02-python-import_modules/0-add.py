2c6dc9ed28e749519bd4c565f35679c7a0c1288e
abelasfaw <abeloman89@gmail.com>
2022-08-25 09:36:59 -0700

    fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-
-
-def func():
-    a = 1
-    b = 2
-    sum = add(a, b)
-    print("{} + {} = {}".format(a, b, sum))
-
+a = 1
+b = 2
+sum = add(a, b)
+print("{} + {} = {}".format(a, b, sum))
