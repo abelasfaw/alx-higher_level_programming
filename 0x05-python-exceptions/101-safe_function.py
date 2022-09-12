f7c74b71941be56cc2f8b84d49da6611028a5682
abelasfaw <abeloman89@gmail.com>
2022-09-12 09:28:06 -0700

    added task 11

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    result = None
+    try:
+        result = fct(args)
+    except BaseException as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return result
+    return result
