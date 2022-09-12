8e11e6d437a0710b0d3dfd32423067fbd4bfed2d
abelasfaw <abeloman89@gmail.com>
2022-09-12 03:15:00 -0700

    fix task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,9 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     while (index < list_length):
         try:
-            div_result = int(my_list_1[index]) / int(my_list_2[index])
+            num1 = convert_to_int_or_float(my_list_1[index])
+            num2 = convert_to_int_or_float(my_list_2[index])
+            div_result = num1 / num2
         except IndexError:
             print("out of range")
         except ValueError:
@@ -17,3 +19,11 @@ def list_division(my_list_1, my_list_2, list_length):
             div_result = 0
             index += 1
     return result
+
+
+def convert_to_int_or_float(input_str):
+    if (isinstance(input_str, int)):
+        return int(input_str)
+    elif (isinstance(input_str, float)):
+        return float(input_str)
+    raise ValueError
