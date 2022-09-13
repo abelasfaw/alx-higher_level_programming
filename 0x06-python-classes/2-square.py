a5e6acdd6a1d015f615aae21750e2c5f1dc40ba7
abelasfaw <abeloman89@gmail.com>
2022-09-13 05:53:50 -0700

    added task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+"""module with a single square class with field validation"""
+
+
+class Square:
+    """Square class with size property"""
+    def __init__(self, size=0):
+        if(not(isinstance(size, int))):
+            raise TypeError("size must be an integer")
+        if(size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
