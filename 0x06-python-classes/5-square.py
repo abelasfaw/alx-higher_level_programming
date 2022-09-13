796c9212b08b1ac3fd395490557695dcbfaa03c5
abelasfaw <abeloman89@gmail.com>
2022-09-13 06:38:52 -0700

    added task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""module with single square class"""
+
+
+class Square:
+    """Square class with size property, area calculating method
+    ,setter and getter"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if(not(isinstance(value, int))):
+            raise TypeError("size must be an integer")
+        if(value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if(self.__size == 0):
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
