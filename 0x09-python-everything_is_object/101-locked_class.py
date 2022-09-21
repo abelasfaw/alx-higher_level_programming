9bc4eae19421372ba69b746c5bbe716f124df119
abelasfaw <abeloman89@gmail.com>
2022-09-21 10:26:09 -0700

    fix task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""module with a single class with restricted dynamic attribure"""
+
+
 class LockedClass:
+    """Class with only single dynamic attribute allowed """
+
     __slots__ = ['first_name']
