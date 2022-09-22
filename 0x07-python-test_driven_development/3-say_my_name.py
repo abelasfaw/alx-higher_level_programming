1444f9d452497c709b2a3f4d4f0e446c5feb656f
abelasfaw <abeloman89@gmail.com>
2022-09-22 14:11:20 -0700

    add documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" module with single name printing function"""
+
+
 def say_my_name(first_name, last_name=""):
+    """prints My name is followed be full name"""
+
     if(not(isinstance(first_name, str))):
         raise TypeError("first_name must be a string")
     if(not(isinstance(last_name, str))):
