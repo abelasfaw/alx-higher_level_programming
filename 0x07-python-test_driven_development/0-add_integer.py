61ec7527ee4efbb5a7232f425975389ce1700c16
abelasfaw <abeloman89@gmail.com>
2022-09-22 13:13:28 -0700

    added task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""adds two integers a and b where a and be are integers or floats,
+raises a TypeError if a or b are of different types"""
+
+
+def add_integer(a, b=98):
+    """returns sum of a and b"""
+
+    if (not(isinstance(a, int) and not(isinstance(a, float)))):
+        raise TypeError("a must be an integer")
+    if (not(isinstance(b, int)) and not(isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    if(isinstance(a, float)):
+        a = int(a)
+    if(isinstance(b, float)):
+        b = int(b)
+    return a + b
