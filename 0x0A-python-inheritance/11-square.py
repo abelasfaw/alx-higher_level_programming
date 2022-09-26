3e80cae11c5b36e1cd356ffdcf3d8200fdb30732
abelasfaw <abeloman89@gmail.com>
2022-09-26 09:43:11 -0700

    added task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    Classes
+    -------
+    Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class to represent a Square
+    ---------------------------
+    Attributes
+    ----------
+    size: int
+    ----------
+    Methods
+    -------
+    area()
+    -------
+    """
+    def __init__(self, size):
+        """initializes Square object"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """calculates area of a square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string representation of square instance"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def __repr__(self):
+        """string representation of square instance"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
