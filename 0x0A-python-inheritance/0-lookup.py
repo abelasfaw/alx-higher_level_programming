c0beacbddc049a08a50d657296eda1b4bf94fcdc
abelasfaw <abeloman89@gmail.com>
2022-09-26 03:31:57 -0700

    added module docs

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""module to check list of attributes and methods of and object"""
+
+
 def lookup(obj):
     """ returns list of attributes and methods of an object"""
     if(obj is not None):
