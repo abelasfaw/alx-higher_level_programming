df2e8905ca034f407224ba36f4bfac2f27a0df47
abelasfaw <abeloman89@gmail.com>
2022-10-03 10:25:34 -0700

    added task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Contains a class that defines Square type"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines Square type"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes a square instance"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Sqaure] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.height)
