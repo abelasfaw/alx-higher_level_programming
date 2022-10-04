f2895f275dd42a2ad16b08c508601cd8033f9304
abelasfaw <abeloman89@gmail.com>
2022-10-03 22:07:05 -0700

    added task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 by other modules in these package"""
 
 
+import json
+
+
 class Base:
     """Base class with private class attribute
     _nb_objects"""
@@ -17,3 +20,9 @@ class Base:
         else:
             Base._nb_objects += 1
             self.id = Base._nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns json string representation of input"""
+        if(list_dictionaries is None or len(list_dictionaries) == 0):
+            return "[]"
+        return json.dumps(list_dictionaries)
