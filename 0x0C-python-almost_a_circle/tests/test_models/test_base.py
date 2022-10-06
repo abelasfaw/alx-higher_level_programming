e5c7ff83fa49c9c30de7c484d8051730e450d438
abelasfaw <abeloman89@gmail.com>
2022-10-06 09:16:44 -0700

    added 5 Base tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,3 +17,23 @@ class TestBase(unittest.TestCase):
         """checkis if id is incremented from previous value"""
         prev = Base().id
         self.assertEqual((prev + 1), Base().id)
+
+    def test_id_save(self):
+        """checks if generated id is saved"""
+        self.assertEqual(Base().id, Base._nb_objects)
+
+    def test_to_json_string_none(self):
+        """validates output with None input"""
+        self.assertEqual("[]", Base().to_json_string(None))
+
+    def test_to_json_string_empty_list(self):
+        """validates output with empty list input"""
+        self.assertEqual("[]", Base().to_json_string([]))
+
+    def test_to_json_string_valid(self):
+        """validates output with expected input"""
+        self.assertEqual('[{"id": 1}]', Base().to_json_string([{'id': 1}]))
+
+    def test_to_json_string_valid_input_type(self):
+        """validates type of output with expected input"""
+        self.assertEqual(type("str"), type(Base().to_json_string([{'id': 1}])))
