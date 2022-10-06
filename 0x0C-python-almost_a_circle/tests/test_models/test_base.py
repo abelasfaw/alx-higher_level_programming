f4822400cc017c1af6ed4dd02a6fe32de5456a5d
abelasfaw <abeloman89@gmail.com>
2022-10-06 07:23:53 -0700

    fix task 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,5 +7,8 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """Tests for class Base"""
+
     def test_auto_id(self):
+        """checks if id is auto generated"""
         self.assertEqual(1, Base().id)
