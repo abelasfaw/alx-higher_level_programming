eb239cf8cb51fa016021501030c394b29399de05
abelasfaw <abeloman89@gmail.com>
2022-12-04 12:12:09 -0800

    added task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' lists all states whose name matches arugment provided'''
+
+
+import sys
+import MySQLdb as mdb
+
+
+if __name__ == "__main__":
+    db = mdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE `name`= '{}'".format(sys.argv[4]))
+    for state in cur.fetchall():
+        print(state)
