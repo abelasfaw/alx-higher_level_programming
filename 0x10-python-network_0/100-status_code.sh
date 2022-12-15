fa4e27ce7b23b1dad87f79b4db34b4022e45d383
abelasfaw <abeloman89@gmail.com>
2022-12-15 13:55:43 -0800

    remove pipes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to passed URL and displays only the status code of the response
-curl -sI "$1" | grep -iF 'http' | cut -d ' ' -f2 | tr -d '\n'
+curl -s "$1" -o /dev/null -w "%{http_code}"
