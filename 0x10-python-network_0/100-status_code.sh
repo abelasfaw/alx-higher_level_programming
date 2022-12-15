a9412bc0754c401da9cfc067e3d4beefbc63b2df
abelasfaw <abeloman89@gmail.com>
2022-12-15 13:53:32 -0800

    task 7 new file

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sends a request to a URL passed as an argument, and displays only the status code of the response.
+#sends a request to passed URL and displays only the status code of the response
 curl -sI "$1" | grep -iF 'http' | cut -d ' ' -f2 | tr -d '\n'
