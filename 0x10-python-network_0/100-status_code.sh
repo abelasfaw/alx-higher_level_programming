376db12e07d88e6e91d43983746b95dfffc92090
abelasfaw <abeloman89@gmail.com>
2022-12-15 13:49:19 -0800

    remove new line from output

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" | grep -iF 'http' | cut -d ' ' -f2 
+curl -sI "$1" | grep -iF 'http' | cut -d ' ' -f2 | tr -d '\n'
