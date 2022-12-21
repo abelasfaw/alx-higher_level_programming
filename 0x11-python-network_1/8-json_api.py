84f497d1f5687ea4bd8b9b3357c277bf8f8f5b44
abelasfaw <abeloman89@gmail.com>
2022-12-21 06:52:28 -0800

    adde task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""takes in a letter and sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.If the response body is properly JSON formatted
+and not empty, display the id and name.Else, Display Not a valid JSON if the
+JSON is invalid, Display No result if the JSON is empty"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    params = {"q": letter}
+    response = requests.post(url, data=params)
+    try:
+        response = response.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid json")
