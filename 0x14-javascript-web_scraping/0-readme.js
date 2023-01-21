007a600306c3987afc306176b5356257a635b737
abelasfaw <abeloman89@gmail.com>
2023-01-21 10:04:36 +0000

    added task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// reads and prints the content of a file
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
