f1a0091806326928d5542b666eeabc52332658b8
abelasfaw <abeloman89@gmail.com>
2023-01-21 15:19:23 +0000

    fix task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,22 +4,24 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-function print (characters) {
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], (error, response, body) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(JSON.parse(body).name);
+function print (characters, element) {
+  request(characters[element], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (element + 1 < characters.length) {
+        print(characters, element + 1);
       }
-    });
-  }
+    }
+  });
 }
+
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
     const characters = JSON.parse(body).characters;
-    print(characters);
+    print(characters, 0);
   }
 });
